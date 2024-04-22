8e0b29a186c841639219cbb9e701c93febb60583
Indie Gabo <95456621+indiegabo@users.noreply.github.com>
2024-04-22 17:25:42 -0300

    removing odin

--- Assets/Tests/Inheritance/ChildState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using HandyFSM;
-using UnityEngine;
-
-public class ChildState : State
-{
-    protected Child ChildMachine => Machine.As<Child>();
-}
