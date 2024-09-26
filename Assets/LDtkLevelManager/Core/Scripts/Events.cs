ae9944943471c4965cab45aa7623f54d45b0760a
Indie Gabo <indiegabodev@gmail.com>
2024-09-25 21:32:34 -0300

    Events

--- Assets/LDtkLevelManager/Core/Scripts/Events.cs
+++ Assets/LDtkLevelManager/Core/Scripts/Events.cs
@@ -3,16 +3,23 @@ using LDtkLevelManager.EventBus;
 namespace LDtkLevelManager
 {
     /// <summary>
-    /// Event that is raised when a level is activated or deactivated.
+    /// Event that is raised when a level is activated.
     /// </summary>
     public class LevelActivationEvent : ILDtkLevelManagerEvent
     {
         /// <summary>
-        /// Whether the level was activated or deactivated.
+        /// The level behaviour that was activated .
         /// </summary>
-        public bool isActive;
+        public LevelBehaviour behaviour;
+    }
+
+    /// <summary>
+    /// Event that is raised when a level is deactivated.
+    /// </summary>
+    public class LevelDeactivationEvent : ILDtkLevelManagerEvent
+    {
         /// <summary>
-        /// The level behaviour that was activated or deactivated.
+        /// The level behaviour that was deactivated.
         /// </summary>
         public LevelBehaviour behaviour;
     }
@@ -26,10 +33,12 @@ namespace LDtkLevelManager
         /// The level behaviour that was prepared.
         /// </summary>
         public LevelBehaviour behaviour;
+
         /// <summary>
         /// The level flow subject from which the level was prepared.
         /// </summary>
         public ILevelFlowSubject subject;
+
         /// <summary>
         /// The trail that was used to prepare the level.
         /// </summary>
